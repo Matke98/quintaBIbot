ab66ec6905050905ee5a91230242448c5fe6b5c5
Matke98 <matke1998@live.it>
2017-08-11 02:25:34 +0200

    Delete bot.php

--- bot.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$token = "417976290:AAFymhc3z62JyTrRJCYc2Jz9XpsKxqF30ok";
-	$website = "https://api.telegram.org/bot" . $token;
-	$send = file_get_contents($website . "/sendMessage?chat_id=417976290&text=ciao");
-	
-	
-
-?>
